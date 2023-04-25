61471cdd50289083462e587e030588dece965d4e
Gaston Recalde <gastonrg94@gmail.com>
2023-04-25 20:42:45 -0300

    REstructuracion del proyecto e inicio de la pagina index hecho

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const navItems = document.querySelector('.nav__link');
+const openNavBtn = document.querySelector('.nav__hamburger');
+const closeNavBtn = document.querySelector('.nav__close');
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+});
+
+if(window.innerWidth < 1024){
+    document.querySelectorAll('.nav__ul li').forEach(navItems => {
+        navItems.addEventListener('click', () => {
+            closeNav();
+        });
+    });
+};
